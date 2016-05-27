1bfce7c146898abbde7a85379be1f6485bf4a2d9
Juan Pol <jpol0191@gmail.com>
2016-05-27 16:16:22 -0400

    Adding root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'users/index'
 
-  # root "user#index"
+  root "users#index"
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.

2b37fcb77f5c5fdfb1361c596766c990c8adcc4e
Juan Pol <jpol0191@gmail.com>
2016-05-31 15:04:14 -0400

    Adding facebook columns to user model

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20160531190535) do
+
+ActiveRecord::Schema.define(version: 20160531190250) do
+
+
 
 
 

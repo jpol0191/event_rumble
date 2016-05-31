62ef91d730d3a5bae1641c8645b88c8043c5f70e
Jian-Lei Hu <whosjianlei@gmail.com>
2016-05-31 15:58:56 -0400

    init

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,19 @@ class User < ActiveRecord::Base
 	has_many :power_ups, dependent: :destroy
 	# ============================================
 
+	#============= Facebook omniauth =============
+	def self.from_omniauth(auth)
+	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	    user.provider 	= auth.provider 
+	    user.uid      	= auth.uid
+	    user.email			= auth.info.email
+	    user.name 			= auth.info.name
+	    user.image			= auth.info.image
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+	    user.save
+	  end
+	end
+	#=============================================	
+
 end

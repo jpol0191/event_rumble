2d08a951af57ba884b9f57586cf0cff8374363ba
Juan Pol <jpol0191@gmail.com>
2016-05-31 15:04:14 -0400

    Adding facebook columns to user model

---
+++ db/migrate/20160531190250_add_facebook_columns_to_user.rb
@@ -0,0 +1,7 @@
+class AddFacebookColumnsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :image, :string
+    add_column :users, :oauth_token, :string
+    add_column :users, :oauth_expires_at, :string
+  end
+end

6af03287bbf807b688990996f98e57cad037d538
Juan Pol <jpol0191@gmail.com>
2016-05-31 20:56:20 -0400

    Adding is_online column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531202251) do
+ActiveRecord::Schema.define(version: 20160601004629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,14 +91,6 @@ ActiveRecord::Schema.define(version: 20160531202251) do
 
   add_index "power_ups", ["user_id"], name: "index_power_ups_on_user_id", using: :btree
 
-  create_table "rooms", force: :cascade do |t|
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "rooms", ["user_id"], name: "index_rooms_on_user_id", using: :btree
-
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
@@ -114,6 +106,7 @@ ActiveRecord::Schema.define(version: 20160531202251) do
     t.string   "oauth_expires_at"
     t.string   "provider"
     t.string   "fullname"
+    t.boolean  "is_online"
   end
 
   add_foreign_key "friends", "users"
@@ -124,5 +117,4 @@ ActiveRecord::Schema.define(version: 20160531202251) do
   add_foreign_key "past_event_users", "past_events"
   add_foreign_key "past_events", "users"
   add_foreign_key "power_ups", "users"
-  add_foreign_key "rooms", "users"
 end

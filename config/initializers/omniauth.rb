d007e9ec6a5bfbbf00cdd59f5b37b785e60e2999
Juan Pol <jpol0191@gmail.com>
2016-06-01 11:22:49 -0400

    Fixing development mode face book initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, FACEBOOK_CONFIG['FACEBOOK_KEY'], FACEBOOK_CONFIG['FACEBOOK_SECRET']
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 end

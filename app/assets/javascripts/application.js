d4c517b7fff8167407161d5825ed39a956084efb
Kassim Ouattara <ouat.kassim@gmail.com>
2016-06-01 14:44:04 -0400

    Styling and finishing the form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,51 +17,25 @@
 
 
 $(document).ready(function(){
-		$('.autofill').hide();
-		var array =[]
-		var i = 0;
-	$('.form').hide();
+	//array of ivited friends
+	$('.autofill').hide();
+	var array =[];
+	//array for autofill
+	var availableTags = []
+	var i = 0;
+	$('.container3').hide();
+
 	$('.create-group').click(function(){
-		$('.form').show();
+		$('.container3').show();
 		$('.create-group').hide();
 	});
-
-
-	// $('.add-friend').click(function(){
-	// 	// find a way to update value on the hidden field!
-	// 	var invited =  $('select').closest('select').find('option:selected').val();
-	// 	array.push(invited)
-	// 	$('#group_fname').val($('#group_fname').val()+','+invited);
-	// 	$('.add-friend').hide();
-	// 	$("#friends option[value='" + invited + "']").hide();
-	// 	if(array[array.length - 1] === array[array.length - 2]){
-	// 		array.pop();
-	// 		$('select').hide();
-	// 		$('.add-friend').hide();
-	// 	}
-	// 	$('.invited-friends').html(array.join(', '));
-	// });
-
-	// $('select').click(function(){
-	// 	$('.add-friend').show();
-	// });
-
-	// $('.clear-friend').click(function(){
-	// 	array =[];
-	// 	$('.invited-friends').html('')
-	// 		$('select').show();
-	// 		$('.add-friend').show();
-	// 		$("#friends option").show();
-	// });
-
-	
 	$("#tags input").on({
     focusout : function() {
       var txt = this.value.replace(/[^a-z0-9\+\-\.\#]/ig,''); // allowed characters
       if(txt) $("<span/>", {text:txt.toLowerCase(), insertBefore:this});
       this.value = "";
       array.push($('span').last().html());
-      console.log(array);
+      // console.log(array);
     },
     keyup : function(ev) {
       // if: comma|enter (delimit more keyCodes with | pipe)
@@ -74,9 +48,17 @@ $(document).ready(function(){
     	if($(this).text() == array[i]){
     		array.pop(i);
     		array.pop(i);
-    		console.log(array)
+    		// console.log(array)
     	}
     }
   });
+
+  $(".autofill").each(function() {
+  	availableTags.push($(this).html());
+  });
+  console.log(availableTags)
+  $( "#tag" ).autocomplete({
+      source: availableTags
+    });
 
 })

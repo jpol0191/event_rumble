2ebea6686cff5230e606669b9bb0098373e2c6b1
Juan Pol <jpol0191@gmail.com>
2016-06-02 11:09:05 -0400

    Adding js to room

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -1,9 +1,29 @@
 $(document).ready(function(){
+	//Variables for DOM elements
+	var categoryListElem = $('#cat-select')	
+
+	// Selected categories are given the class of "active"
+	$('.category-select-indicator').on('click', function(e){
+		if ($(this).parent().hasClass('active')){
+			$(this).parent().removeClass('active');
+			$(this).removeClass('active-indicator');
+		}else{
+			$(this).parent().addClass('active');
+			$(this).addClass('active-indicator');
+		}	
+	});
+
+	// Calls the eventful api and displays the results on the page
 	$('#event-search').on('click', function(){
-		var c = $('#category_selected').val();
+		// var c = $('#category_selected').val();
+		var arry = [];
 		var d = $('#date_selected').val();
 		
-
+		for ( var i=0; i < categoryListElem.children('.active').length; i++){
+			arry.push(categoryListElem.children('.active')[i].getAttribute('value'));
+			c = arry.join(',')
+		}
+		console.log(c)
 		$.ajax({   
 			url: "http://api.eventful.com/json/events/search?callback=?",   
 			data: {     
@@ -22,7 +42,7 @@ $(document).ready(function(){
 					elem.append('<li>'+'<h4>'+ events[i].title+ '</h4>' +'</li>');
 
 					// Show image if valid url exist
-					elem.children().last().append('<img src="'+ (events[i].image ? events[i].image.medium.url : "/assets/no-picture.png" )+'">');
+					elem.children().last().append('<img src="'+ (events[i].image ? events[i].image.medium.url : "/assets/no-picture.png" ) +'">');
 
 					// Show description, if description is invald display "No description"
 					elem.children().last().append('<p>'+ (events[i].description ? events[i].description: "No description" ) + '</p>')
@@ -30,4 +50,7 @@ $(document).ready(function(){
 			} 
 		});
 	})
+
+
+
 })

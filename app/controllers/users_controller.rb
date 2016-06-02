9fba1918a097b1f3d280579f80ea6db26d3b1a82
Jian-Lei Hu <whosjianlei@gmail.com>
2016-06-02 17:59:00 -0400

    userflow good to go

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,27 @@
 class UsersController < ApplicationController
   def index
-  	@users=User.all
+    @users=User.all
     @user=User.new
+    if current_user
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def create
 
     @user = User.new(user_params)
+    @user.image = "http://u.o0bc.com/avatars/no-user-image.gif"
       if @user.save
-        redirect_to root_path
+        redirect_to user_path(current_user.id)
       else
         redirect_to sessions_path
       end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def show
      @user = User.find(params[:id])
      @users = User.order("fname ASC").all
@@ -23,18 +32,25 @@ class UsersController < ApplicationController
   end
 
   def destroy
-  	@user=User.find(params[:id])
-  	@user.destroy
+    @user=User.find(params[:id])
+    @user.destroy
+    session.clear
+    redirect_to root_path
   end
 
   def update
-  	@user=User.find(params[:id])
-  	@user.update(user_params)
+    @user=User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      redirect_to edit_user_path(@user)
+    end
   end
   
   private
 
   def user_params
-  	params.require(:user).permit(:email,:password,:phone,:fname,:lname)
+    params.require(:user).permit(:email,:password,:phone,:fname,:lname, :image,:password_confirmation)
   end
 end

5de1ac13bdbc5ad373972d207924c82982abc8f6
Juan Pol <jpol0191@gmail.com>
2016-06-02 08:38:45 -0400

    Cleaning the js folder and adding eventful api function

--- app/helpers/categorys_helper.rb
+++ app/helpers/categorys_helper.rb
@@ -1,2 +1,10 @@
 module CategorysHelper
+
+	def categories_list
+		["music","food","movies","comedy","art","technology"]
+	end
+
+	def date_options
+		["future","today","this week", "next week"]
+	end
 end

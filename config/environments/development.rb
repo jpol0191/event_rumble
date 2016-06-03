b5b701e498cc49d0b50bbc6fa897413525498794
Juan Pol <jpol0191@gmail.com>
2016-06-03 13:41:46 -0400

    Adding websockets internal error fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Fixes internal server error when working with websockets 
+  config.middleware.delete Rack::Lock
 end
